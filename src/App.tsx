ebb9ea65ce57444a4962fc30be9447831edd4335
Egor Bazakin <egor.bazakin.09@gmail.com>
2026-04-10 20:32:18 +0300

    feat: widgets folder with initial header setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import styles from './App.module.css';
 import { AppRouter } from './app/router';
+import { Header } from './widgets/header';
 
 const App = () => {
   return (
     <div className={styles.shell}>
+      <Header />
       <main className={styles.main}>
         <AppRouter />
       </main>
---
+++ src/widgets/header/ui/Header.tsx
@@ -0,0 +1,23 @@
+import { classNames } from '@/shared/lib/className';
+import styles from './Header.module.css';
+import { NavLink } from 'react-router-dom';
+import { ROUTES } from '@/app/router';
+
+const getLinkClassName = ({ isActive }: { isActive: boolean }) => {
+  return classNames(styles.link, isActive && styles.activeLink);
+};
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav} aria-label="Primary">
+        <NavLink to={ROUTES.cats} className={getLinkClassName}>
+          Все котики
+        </NavLink>
+        <NavLink to={ROUTES.favorites} className={getLinkClassName}>
+          Любимые котики
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
